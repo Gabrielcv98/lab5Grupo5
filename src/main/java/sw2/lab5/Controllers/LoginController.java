f2762cb9d75cbfef3c4d57bd26636e27583270f8
Eric Benites <eric.benites@pucp.edu.pe>
2020-05-18 20:02:54 -0500

    controller user

---
+++ src/main/java/sw2/lab5/Controllers/LoginController.java
@@ -0,0 +1,13 @@
+package sw2.lab5.Controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+
+
+}
